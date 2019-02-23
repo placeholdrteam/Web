27e6d74293ff862f8cf750899d8fe0a174d1a486
Sundeep Kakar <skakar@iu.edu>
2019-02-23 10:43:50 -0500

    Added Working React-Route

--- placeholdr/src/components/user/Profile.js
+++ placeholdr/src/components/user/Profile.js
@@ -16,13 +16,12 @@ class Profile extends React.Component
                     </div>
                     <div className="col s4">Description</div>
                     <div className="col s10">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum.
+                        
                     </div>
                     <div className="col s6">
                         INSERT_TAGS_TABLE
                     </div>
                 </div>
-                
             </div>
         );
     }
