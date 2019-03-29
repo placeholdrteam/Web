f4f333d7c6c5ff49d326ef427bceb1c10fecb56f
Daniel Peach <dpeaches96@gmail.com>
2019-03-29 12:34:12 -0400

    added thunk, firestore, and firebase store enhancers

--- placeholdr/src/index.js
+++ placeholdr/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase'
+import { getFirestore, reduxFirestore } from 'redux-firestore'
+import firebaseConfig from './config/firebase'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(firebaseConfig),
+    reduxFirestore(firebaseConfig)
+))
+
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'))
---
+++ placeholdr/src/store/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
+
+const rootReducer = combineReducers({
+  fb: firebaseReducer,
+  db: firestoreReducer
+})
+
+export default rootReducer
